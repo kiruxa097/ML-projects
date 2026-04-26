e6d66744879c8c5f048fed4f24162e53a5778270
Kirill <mordvinkin2740@gmail.com>
2026-04-26 12:14:05 +0300

    Знакомство с бибилиотекой sklearn, dataset: iris

--- iris_project/iris1.py
+++ iris_project/iris1.py
@@ -1,1 +1,10 @@
+from sklearn.datasets import load_iris
 
+iris = load_iris()
+
+print("Названия признаков: ", iris.feature_names)
+print("Названия сортов: ", iris.target_names)
+print("Количество цветков: ", len(iris.data))
+print("Признаки первого цветка: ", iris.data[0])
+print("Сорт первого цветка(число): ", iris.target[0])
+print("Сорт первого цветка(строка(название)): ", iris.target_names[iris.target[0]])
